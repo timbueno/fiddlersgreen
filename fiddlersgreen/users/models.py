b8c1596d07d00c2ede7d91285a1e9f9631e961c6
Tim Bueno <timbueno@gmail.com>
2015-03-28 13:35:13 -0400

    added full user model

--- fiddlersgreen/users/models.py
+++ fiddlersgreen/users/models.py
@@ -6,8 +6,11 @@
     User models
 
 """
-from ..core import Model
+from ..core import db, Model
 
 
 class User(Model):
     __tablename__ = 'users'
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
