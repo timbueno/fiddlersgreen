778f78ca74f3ad76eb3e1b830c5303c5d8cdae52
Tim Bueno <timbueno@gmail.com>
2015-03-31 18:18:38 -0400

    added Flask-Bcrypt for password hashing

--- fiddlersgreen/core.py
+++ fiddlersgreen/core.py
@@ -20,6 +20,9 @@ migrate = Migrate()
 from flask.ext.login import LoginManager
 login_manager = LoginManager()
 
+from flask.ext.bcrypt import Bcrypt
+bcrypt = Bcrypt()
+
 
 class CRUDMixin(object):
     """
