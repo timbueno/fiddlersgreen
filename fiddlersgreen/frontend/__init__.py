c171d76cae314127248695bc91ff2e7603a6676a
Tim Bueno <timbueno@gmail.com>
2015-04-01 21:33:01 -0400

    Registered flask-bootstrap on the frontend app

--- fiddlersgreen/frontend/__init__.py
+++ fiddlersgreen/frontend/__init__.py
@@ -9,13 +9,21 @@
 from functools import wraps
 
 from .. import factory
+from ..core import bootstrap
 
 
 def create_app(config):
     """Returns the frontend application instance"""
     app = factory.create_app(__name__, __path__, config)
 
+    register_extensions(app)
+
     return app
+
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    return None
 
 
 def route(bp, *args, **kwargs):
