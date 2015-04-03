7918f61e59c29618a15c89dbbb4eac37f467df5f
Tim Bueno <timbueno@gmail.com>
2015-04-02 21:04:42 -0400

    basic auth and token auth are both now supported with the authorization header

--- fiddlersgreen/api/auth.py
+++ fiddlersgreen/api/auth.py
@@ -20,11 +20,28 @@ bp = Blueprint('auth', __name__)
 @login_manager.request_loader
 def load_user_from_request(request):
     # First try to load user from api key url arg
-    api_key = request.args.get('api_key')
-    if api_key:
-        user = User.verify_auth_token(api_key)
-        if user:
+    # api_key = request.headers.get('Authorization')
+    auth = request.authorization
+    if auth:
+        email_or_token = auth.username
+        password = auth.password
+    else:
+        email_or_token = ''
+        password = ''
+    # Check for email or token, abort if not provided
+    if email_or_token == '':
+        return None
+    # If email_or_token is set but password is not, run the token auth
+    if password == '':
+        user = User.verify_auth_token(email_or_token)
+        return user
+
+    # Try basic authentication
+    user = User.query.filter_by(email=email_or_token).first()
+    if user:
+        if user.verify_password(password):
             return user
+
     # Finally, return None if no user was logged in
     return None
 
